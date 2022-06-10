92d36a5288096e29a99cc2b7ec8e8cc4d241b88d
IsawU <jiri.jurecek@seznam.cz>
2022-06-10 20:15:07 +0200

    Clean up old code

--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,22 @@
 import * as vscode from 'vscode';
 
-// https://code.visualstudio.com/api/references/vscode-api#CompletionItem
-// https://code.visualstudio.com/api/references/vscode-api#CompletionItemKind
-// https://code.visualstudio.com/api/references/vscode-api#TextDocument.getWordRangeAtPosition
-// TODO: Possibly avoid Snippet CompletionItemKind.
+
 
 export function activate(context: vscode.ExtensionContext) {
 	let skip = false;
 
 	const provider = vscode.languages.registerCompletionItemProvider({scheme: 'file'}, {
 		async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-			// Skip every other invokation.
-			if (skip) {
-				skip = false;
-				return [];
-			}
-			skip = true;
+			// Skip every other invokation to prevent infinite loops. Looking for a better solution.
+			skip = !skip;
+			if (!skip) return [];
 
 			const editor = vscode.window.activeTextEditor;
-			if (editor === undefined) {
-				return [];
-			}
+			if (editor === undefined) return [];
 
 			const srcRange = editor.document.getWordRangeAtPosition(position);
-			if (srcRange === undefined) {
-				return [];
-			}
-			const range = new vscode.Range(srcRange?.start, position);
+			if (srcRange === undefined) return [];
+			const range = new vscode.Range(srcRange.start, position);
 			const word = editor.document.getText(range);
 
 			const completionList: vscode.CompletionList = await vscode.commands.executeCommand('vscode.executeCompletionItemProvider', document.uri, position);
@@ -57,49 +47,8 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(provider);
 }
 
-export function deactivate() {}
 
-interface CompletionTree {
-	[key: string]: string[] | CompletionTree,
-}
 
-// CREATE A TREE
-function processIntoTree(node: string[], chars: number = 0): CompletionTree {
-	const result: CompletionTree = {};
-
-	if (node.length == 1) {
-		result[node[0]] = {};
-	}
-	else {
-		const initialChars = chars;
-		while (node.length > 0) {
-			const reference = node[0];
-			chars = initialChars + 1;
-			let items: string[] = node.filter(item => item.startsWith(reference.slice(0, chars)));
-			chars += 1;
-			for (; chars <= reference.length && items.length > 1; chars++) {
-				let new_items = items.filter(item => item.startsWith(reference.slice(0, chars)));
-				if (new_items.length < items.length) {
-					console.log(new_items);
-					console.log('<');
-					console.log(items);
-					break;
-				}
-				items = new_items;
-			}
-			chars -= 1;
-			node = node.filter(item => !items.includes(item));
-
-			const tag = items.length > 1 ? items[0].slice(0, chars) : reference;
-			items = items.filter(item => item !== tag);
-			result[tag] = processNode(items, chars);
-		}
-	}
-
-	return result;
-}
-
-// Meticulouslt select only the partials that we need.
 function processNode(node: string[], chars: number = 0): string[] {
 	const result: string[] = [];
 
